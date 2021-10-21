a69392864d85b7c52e4bbf5d444ea12a266a7e20
Godwin <67944336+UgbeGodwinIkpe@users.noreply.github.com>
2021-10-21 23:48:13 +0100

    setting the php code in the index

--- subscriber.php
+++ subscriber.php
@@ -1,18 +1,14 @@
 <?php
 
 
-if(isset($_POST["submit"])) {
+if (isset($_POST["submit"])) {
 
-$to = "ugbegodwin7963@gmail.com";
-$from = $_POST['email'];
-$name = $_POST['name'];
-$subject = $_POST['Message from my Portfolio'];
-$message = $_POST['message'];
-$header = $_POST['From:' . $from];
-mail($to, $subject,$message, $header );
-echo "Message sent. Thank you " . $name
-
+     $to = "ugbegodwin7963@gmail.com";
+     $from = $_POST['email'];
+     $name = $_POST['name'];
+     $subject = $_POST['Message from my Portfolio'];
+     $message = $_POST['message'];
+     $header = $_POST['From:' . $from];
+     mail($to, $subject, $message, $header);
+     echo "Message sent. Thank you " . $name;
 }
-
-
-?>
