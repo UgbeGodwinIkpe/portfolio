42d1dc8244fee4c584e00fa5f06dac1430d79c63
Godwin <67944336+UgbeGodwinIkpe@users.noreply.github.com>
2021-10-28 17:31:38 +0100

    still fixing the form contact

--- subscriber.php
+++ subscriber.php
@@ -2,17 +2,18 @@
 
 var_dump($_POST);
 exit;
-if ($_POST["submit"]) {
-
-     $to = "ugbegodwin7963@gmail.com";
-     $from = $_POST['email'];
-     $name = $_POST['name'];
-     $subject = $_POST['Message from my Portfolio'];
-     $message = $_POST['message'];
-     $header = $_POST['From:' . $from];
-     if (mail($to, $subject, $message, $header)) {
-          echo "Message sent. Thank you " . $name;
-     } else {
-          echo "Unable to send your message. Please try again";
+if ($_POST["email"] && $_POST["message"]) {
+     if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+          $to = "ugbegodwin7963@gmail.com";
+          $from = $_POST['email'];
+          $name = $_POST['name'];
+          $subject = $_POST['My Portfolio Contact Form'];
+          $message = $_POST['message'];
+          $header = $_POST['From:' . $from];
+          if (mail($to, $subject, $message, $header)) {
+               echo "Message sent. Thank you " . $name;
+          } else {
+               echo "Unable to send your message. Please try again";
+          }
      }
 }
