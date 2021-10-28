9e6bd5f395e61090c287def40b2c2bb9d2409f0f
Godwin <67944336+UgbeGodwinIkpe@users.noreply.github.com>
2021-10-28 15:44:26 +0100

    Fixing the form

--- subscriber.php
+++ subscriber.php
@@ -1,7 +1,8 @@
 <?php
 
-
-if (isset($_POST["submit"])) {
+var_dump($_POST);
+exit;
+if ($_POST["submit"]) {
 
      $to = "ugbegodwin7963@gmail.com";
      $from = $_POST['email'];
@@ -9,6 +10,9 @@ if (isset($_POST["submit"])) {
      $subject = $_POST['Message from my Portfolio'];
      $message = $_POST['message'];
      $header = $_POST['From:' . $from];
-     mail($to, $subject, $message, $header);
-     echo "Message sent. Thank you " . $name;
+     if (mail($to, $subject, $message, $header)) {
+          echo "Message sent. Thank you " . $name;
+     } else {
+          echo "Unable to send your message. Please try again";
+     }
 }
